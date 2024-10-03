433633ab489860258a6d28a677eac12681926a80
letruxux <mrgianfranco483@gmail.com>
2024-10-03 21:02:50 +0200

    prettier transition opacity on floating footer

--- packages/frontend/src/components/FloatingFooter.tsx
+++ packages/frontend/src/components/FloatingFooter.tsx
@@ -1,11 +1,19 @@
 export default function FloatingFooter() {
   return (
     <footer className="fixed bottom-4 left-0 w-full p-2 flex justify-center text-sm">
-      <a href="https://github.com/letruxux/srch" target="_blank" className="link">
+      <a
+        href="https://github.com/letruxux/srch"
+        target="_blank"
+        className="link hover:opacity-90 transition-opacity"
+      >
         Github
       </a>
       <span className="mr-1 ml-1">•</span>
-      <a href="https://ko-fi.com/letruxux" target="_blank" className="link">
+      <a
+        href="https://ko-fi.com/letruxux"
+        target="_blank"
+        className="link hover:opacity-90 transition-opacity"
+      >
         Donate
       </a>
     </footer>
