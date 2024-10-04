0158db26027e26a80be23f9f5dda64a29fab71ec
letruxux <mrgianfranco483@gmail.com>
2024-10-04 18:08:48 +0200

    add fallback google search to steam rip

--- packages/backend/src/providers/steamrip.ts
+++ packages/backend/src/providers/steamrip.ts
@@ -1,6 +1,8 @@
 import * as cheerio from "cheerio";
 import type BaseResult from "shared/defs";
 import type { ProviderExports } from "shared/defs";
+import { search, OrganicResult, ResultTypes } from "google-sr";
+import { fetchPage } from "../utils";
 
 const baseUrl = "https://steamrip.com/";
 
@@ -14,33 +16,96 @@ export function generateUrl({ query }: { query: string }) {
   return urlString;
 }
 
-export function parsePage(page: string): BaseResult[] {
-  const $ = cheerio.load(page);
-  const results = $("#masonry-grid div.post-element");
+async function mainSearch(url: string): Promise<BaseResult[] | null> {
+  try {
+    const page = await fetchPage(url);
+    const $ = cheerio.load(page);
+    const results = $("#masonry-grid div.post-element");
+    const dataResults: BaseResult[] = [];
+
+    results.each((_, el) => {
+      try {
+        const title = $(el)
+          .find("div.slide a .screen-reader-text")
+          .text()
+          .split("Free Download")[0]!
+          .trim();
+        const link = baseUrl + $(el).find("div.slide a").attr("href")!.trim();
+        const icon = undefined; //$(el).find("div.slide").attr("data-back")!;
+        console.log(`Parsed result - Title: ${title}, Link: ${link}, Icon: ${icon}`);
+        dataResults.push({
+          title,
+          link,
+          icon,
+        });
+      } catch (e) {
+        console.error("Skipping element due to error:", e);
+      }
+    });
+
+    console.log(`Parsed ${dataResults.length} results.`);
+    return dataResults;
+  } catch (e) {
+    console.error("Error in mainSearch:", e);
+    return null;
+  }
+}
+
+async function secondaryGoogleSearch(ogQuery: string): Promise<BaseResult[]> {
+  const query = `site:${new URL(baseUrl).hostname} ${ogQuery}`;
+  const queryResult = await search({
+    query,
+    resultTypes: [OrganicResult],
+  });
+
   const dataResults: BaseResult[] = [];
 
-  results.each((_, el) => {
-    try {
-      const title = $(el)
-        .find("div.slide a .screen-reader-text")
-        .text()
-        .split("Free Download")[0]!
+  queryResult.forEach((result) => {
+    /* oh gosh why is this so bad... */
+    if (
+      result.type === ResultTypes.OrganicResult &&
+      result.link &&
+      result.title &&
+      result.title.toLowerCase().includes("free download")
+    ) {
+      const title = result.title
+        .replace(" Free Download", "")
+        .replace(" - SteamRIP", "")
+        .replace(/\s+/g, " ")
         .trim();
-      const link = baseUrl + $(el).find("div.slide a").attr("href")!.trim();
-      const icon = undefined; //$(el).find("div.slide").attr("data-back")!;
-      console.log(`Parsed result - Title: ${title}, Link: ${link}, Icon: ${icon}`);
-      dataResults.push({
+      const link = result.link;
+      const icon = undefined;
+      const data: BaseResult = {
         title,
         link,
         icon,
-      });
-    } catch (e) {
-      console.error("Skipping element due to error:", e);
+      };
+      dataResults.push(data);
+    } else {
+      console.log(`Skipping result: ${result.title}`);
     }
   });
 
-  console.log(`Parsed ${dataResults.length} results.`);
+  // TODO
   return dataResults;
+}
+
+export async function fetchResults(url: string): Promise<BaseResult[]> {
+  const main = await mainSearch(url);
+
+  if (main) {
+    return main;
+  }
+
+  /* use google query instead */
+  const query = new URL(url).searchParams.get("s");
+  if (!query) {
+    throw new Error("Search failed, fallback method failed as well.");
+  }
+
+  const googleResults = await secondaryGoogleSearch(query);
+
+  return googleResults;
 }
 
 export default {
@@ -50,6 +115,6 @@ export default {
   name: "SteamRIP",
   category: "Games",
 
-  parsePage,
+  fetchResults,
   generateUrl,
 } as ProviderExports;
