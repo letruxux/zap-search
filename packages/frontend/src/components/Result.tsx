0e5ede9fe6867a139a8949eef1e15588e0c2377d
letruxux <mrgianfranco483@gmail.com>
2024-10-07 14:23:00 +0200

    finally fix styling 🙏🙏🙏

--- packages/frontend/src/components/Result.tsx
+++ packages/frontend/src/components/Result.tsx
@@ -1,8 +1,6 @@
 import { FinalResult, ProviderInfo } from "shared/defs";
 import { icons } from "../icons";
 import ImageWithPopup from "./Image";
-import HoverTooltip from "./HoverTooltip";
-import { useEffect, useRef, useState } from "react";
 import clsx from "clsx";
 import IconsComponent from "./IconsComponent";
 
@@ -15,9 +13,6 @@ export default function Result({
   providers: ProviderInfo[];
   highlight?: boolean;
 }) {
-  const titleRef = useRef<HTMLHeadingElement>(null);
-  const [truncateText, setTruncateText] = useState<boolean>(false);
-
   const fixedTitle =
     result.title
       .replace(/^\.+\s*/, "") // Remove leading dots and spaces
@@ -28,24 +23,12 @@ export default function Result({
 
   const provider = providers.find((p) => p.id === result.provider)!;
 
-  useEffect(() => {
-    function updateTruncate() {
-      const mustTruncate = (titleRef.current?.clientHeight ?? 0) > 32;
-      console.log("Truncating", mustTruncate);
-      if (truncateText !== mustTruncate) setTruncateText(mustTruncate);
-    }
-
-    window.onresize = () => updateTruncate();
-
-    updateTruncate();
-  }, []);
-
   return (
     <div className="card transition-shadow bg-base-300 shadow-xl hover:shadow-2xl duration-300 mt-4 first:mt-2">
-      <div className="card-body flex flex-col sm:flex-row items-start sm:items-center">
+      <div className="card-body flex flex-col sm:flex-row items-start sm:items-center justify-between">
         {/* icon */}
         {result.icon && (
-          <div className="h-16 mb-4 w-auto sm:aspect-square sm:mb-0 sm:mr-4">
+          <div className="h-16 w-auto flex-shrink-0 mb-4 sm:mb-0 sm:mr-4">
             <ImageWithPopup
               src={result.icon}
               alt={result.title}
@@ -55,27 +38,13 @@ export default function Result({
         )}
 
         {/* title and info */}
-        <div className="flex-grow w-full sm:max-w-[calc(100%-10rem)]">
+        <div className="flex-grow w-full sm:max-w-[calc(100%-16rem)]">
           <h2
             className={clsx("card-title text-xl sm:text-2xl font-bold break-words", {
               "text-primary": highlight,
-              truncate: truncateText,
             })}
-            ref={titleRef}
           >
-            {truncateText ? (
-              <HoverTooltip
-                tooltipText={
-                  <span className="font-normal break-normal text-base">
-                    {result.title}
-                  </span>
-                }
-              >
-                {fixedTitle}
-              </HoverTooltip>
-            ) : (
-              <span>{fixedTitle}</span>
-            )}
+            <span>{fixedTitle}</span>
           </h2>
           <p className="text-sm text-base-content opacity-70 mt-2">
             <div className="flex items-center flex-row space-x-2">
@@ -85,8 +54,6 @@ export default function Result({
         </div>
 
         {/* button */}
-        {/* fuck this button 
-        it gets offset when there is an image i cant fix it dawg im so mad */}
         <div className="card-actions justify-start sm:justify-end mt-4 sm:mt-0 w-full sm:w-auto">
           <a
             href={result.link}
@@ -95,7 +62,7 @@ export default function Result({
             className="w-full sm:w-auto"
           >
             <button
-              className={clsx("btn w-full sm:w-[8.735rem]", {
+              className={clsx("btn w-full sm:min-w-[140px] sm:w-auto", {
                 "btn-primary": highlight,
                 "btn-outline": !highlight,
               })}
