126565d0735c10e8c0d4f638d0ad597837cb6d6b
letruxux <mrgianfranco483@gmail.com>
2024-10-14 17:25:52 +0200

    temporary websearch online fix

--- packages/backend/src/providers/ofme.ts
+++ packages/backend/src/providers/ofme.ts
@@ -1,6 +1,7 @@
 import * as cheerio from "cheerio";
 import type BaseResult from "shared/defs";
 import type { ProviderExports } from "shared/defs";
+import { fetchPage, webSearch } from "../utils";
 
 const baseUrl = "https://online-fix.me/index.php";
 
@@ -12,7 +13,7 @@ export function generateUrl({ query }: { query: string }) {
   return urlString;
 }
 
-export function parsePage(page: string): BaseResult[] {
+function parsePage(page: string): BaseResult[] {
   const $ = cheerio.load(page);
   const results = $(".news.news-search .article.clr");
   const dataResults: BaseResult[] = [];
@@ -22,6 +23,7 @@ export function parsePage(page: string): BaseResult[] {
       const title = $(el).find("h2.title").text().trim().slice(0, -8);
       const link = $(el).find(".image a").attr("href")!.trim();
       const icon = undefined; //$(el).find("img.lazyload").attr("data-src")!;
+      /* dont use icon since it has returns a hotlink warning */
 
       dataResults.push({
         title,
@@ -36,6 +38,20 @@ export function parsePage(page: string): BaseResult[] {
   return dataResults;
 }
 
+async function fetchResults(url: string) {
+  try {
+    const html = await fetchPage(url);
+
+    return parsePage(html);
+  } catch (e) {
+    console.error("Error while processing, using fallback.", e);
+
+    const query = new URL(url).searchParams.get("s");
+    const results = await webSearch(`site:${new URL(baseUrl).hostname} ${query}`);
+    return results;
+  }
+}
+
 export default {
   baseUrl,
   action: "Download",
@@ -45,6 +61,6 @@ export default {
   category: "Games",
   possibleDownloadTypes: ["direct", "torrent"],
 
-  parsePage,
+  fetchResults,
   generateUrl,
 } as ProviderExports;
